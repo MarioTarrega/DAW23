ba7bf2319b4844c496972aabfbc11bca376d0091
Mario Tarrega <mariotarrega4@gmail.com>
2023-11-09 15:24:17 +0100

    mos

--- DAW 2º/DWS/Primer Trimestre/Ejercicios/Ejerc13/menu.php
+++ DAW 2º/DWS/Primer Trimestre/Ejercicios/Ejerc13/menu.php
@@ -4,7 +4,7 @@
 
     if(isset($_SESSION['usuario'])){
         $user = $_SESSION['usuario'];
-        echo"<p>Bienvenido/a/e '${user}. Seleccione la opción deseada</p>";
+        echo"<p>Bienvenido/a/e '${user}' Seleccione la opción deseada</p>";
         echo"<input type= >";
     }
 
