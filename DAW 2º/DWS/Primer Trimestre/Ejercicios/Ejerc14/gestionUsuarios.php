5938cd4632e5e61b199c97ba1fd3b8f4b724488f
MarioTarrega <145040216+MarioTarrega@users.noreply.github.com>
2023-12-05 09:17:05 +0100

    mod

--- DAW 2º/DWS/Primer Trimestre/Ejercicios/Ejerc14/gestionUsuarios.php
+++ DAW 2º/DWS/Primer Trimestre/Ejercicios/Ejerc14/gestionUsuarios.php
@@ -1,37 +1,28 @@
 <?php
-session_start();
-require_once('conectar.php');
+    session_start();
+    require_once('conectar.php');
 
+    $cnc = conectarBBDD();
+        
     if(isset($_POST['submit'])){
         $user = $_POST['nombre'];
         $pass = $_POST['passw'];
         $tipo = $_POST['tipo'];
-
-        $pass_cif = password_hash($pass, PASSWORD_DEFAULT);
-
-        $sql = " INSERT into usuarios (nombre, pwd, tipo) VALUES ($user, $pass_cif, $tipo)";
-        $cnc = conectarBBDD();
-    
+        
+        $user_cls = limpiarCaracteres($cnc, $user);
+        $pass_cls = limpiarCaracteres($cnc, $pass);
+        $pass_cif = password_hash($pass_cls, PASSWORD_DEFAULT);
+            print_r($user_cls);
+        $sql = " INSERT into usuarios (nombre, pwd, tipo) VALUES ($user_cls, $pass_cif, $tipo)";
+        
         if($cnc){
             $result = mysqli_query($cnc, $sql);  
         }
-            
+                
     }
 
-    $sql = " INSERT into usuarios (nombre, pwd, tipo) VALUES ($user, $pass, $tipo)";
-    $cnc = conectarBBDD();
-
-    if($cnc){
-        $result = mysqli_query($cnc, $sql);  
-    }
+    
+   //$NUsuario = limpiarCaracteres($conex,$usuario);
+    //$pasw = limpiarCaracteres($conex,$_POST['passw']);
     mysqli_close(conectarBBDD());
-    
-    if(isset($_SESSION['nombre'])){
-        $nombre = $_SESSION['nombre'];
-        $usuario = $_POST['nombre'];
-        echo "$nombre, El usuario $usuario ha sido introducido.";
-    }
-    
-    $NUsuario = limpiarCaracteres($conex,$usuario);
-    $pasw = limpiarCaracteres($conex,$_POST['passw']);
 ?>
