5c6782ddb593129178d064ef9f1a14168091978f
MarioTarrega <mariotarrega4@gmail.com>
2023-12-14 16:29:05 +0100

    Mod Artistas

--- DAW 2º/DWS/Primer Trimestre/Ejercicios/Ejerc14/mostrarArtistas.php
+++ DAW 2º/DWS/Primer Trimestre/Ejercicios/Ejerc14/mostrarArtistas.php
@@ -6,9 +6,41 @@ if(isset($_SESSION['nombre'])){
     $usuario = $_SESSION['nombre'];
     echo "$usuario, los usuarios dados de alta son:";
 }
+$conex = conectarBBDD();
 
-$sql ="SELECT nombre FROM usuarios";
-$resultado=mysqli_query(conectarBBDD(),$sql);
+function obtenerArtistas($conex){
+    $sql = "SELECT * FROM artistas";
+    $resultado = mysqli_query($conex, $sql);
+
+    $artistas = array();
+    while ( $fila = mysqli_fetch_assoc($resultado)){
+        $artistas[] = $fila;
+    }
+    return $artistas;
+}
+
+function obtenerGrupos($conex, $idArtista){
+    $sql = "SELECT * FROM grupos INNER JOIN integrantes ON grupos.idgrupo = integrantes.idgrupo
+            WHERE idartista = $idArtista";
+    $resultado = mysqli_query($conex,$sql);
+
+    $grupos = array();
+    while( $fila = mysqli_fetch_assoc($resultado)){
+        $grupos[] = $fila;
+    }
+    return $grupos;
+}
+
+function obtenerDiscos($conex, $idGrupo){
+    $sql = "SELECT * FROM discos WHERE idgrupo = $idGrupo";
+    $resultado = mysqli_query($conex, $sql);
+
+    $discos = array();
+    while( $fila = mysqli_fetch_assoc($resultado)){
+        $discos[] = $fila;
+    }
+    return $discos;
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -19,14 +51,27 @@ $resultado=mysqli_query(conectarBBDD(),$sql);
 </head>
 <body>
     <?php
-    if($resultado){
-        echo"<ul>";
-        while ($row = mysqli_fetch_array($resultado)) {
-            echo "<li>" . $row["nombre"]."</li>";  //muestra el nombre del usuario
+    $artistas = obtenerArtistas($conex);
+    foreach($artistas as $artista){
+        echo "Nombre: " . $artista['nombre'] ." <br>";
+        echo "Nacionalidad: " . $artista['nacionalida'] . "<br>";
+        echo "Instrumento: " . $artista['instrumento'] . "<br>";
+        echo "Biografia: " . $artista['biografia'] . "<br>";
+        echo "Sitio Web: " . $artista['website'] . "<br>";
+        
+        $grupos = obtenerGrupos($conex, $artista['idartista']);
+
+        foreach ($grupos as $grupo){
+            echo "<br>Grupo: " . $grupo['Nombre'] . "<br>";
+            
+            $discos = obtenerDiscos($conex, $grupo['idgrupo']);
+
+            foreach ($discos as $disco){
+                echo "Discos: " . $disco['nombre'] . "<br>";
+            }
         }
-        echo "</ul>";
-    }else{
-        echo "No hay resultados";
+
+        echo "<hr>";
     }
     mysqli_close(conectarBBDD());
     ?>
