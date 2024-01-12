f15d247fa81574a489586cdedbe6eba247456252
Mario Tarrega <mariotarrega4@gmail.com>
2024-01-12 07:49:39 +0100

    12/01

--- DAW 2º/DWS/Primer Trimestre/Ejercicios/Ejerc14/gestionDiscos.php
+++ DAW 2º/DWS/Primer Trimestre/Ejercicios/Ejerc14/gestionDiscos.php
@@ -12,4 +12,48 @@
         $discoSelec = $_POST['seleccionaD'];
     }
 
+    function obtenerDiscoG($conex, $grupoSelec){
+        $sql="SELECT * FROM disco WHERE idgrupo='$grupoSelec'";
+        $consulta = mysqli_query($conex, $sql);
+
+        $discoG = array();
+        while ($fila=mysqli_fetch_array($consulta)) {
+            $discoG[]=$fila;
+        }
+        return $discoG;
+    }
+
+    function obtenerDiscoD($conex, $discoSelec){
+        $sql="SELECT * FROM discos WHERE iddisco = $discoSelec";
+    }
+
+    
+
+
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestion Discos</title>
+</head>
+<body>
+    <?php
+        
+            $sql = "SELECT * FROM discos WHERE 1 + 1 ";
+            if($grupoSelec){
+                $sql .= "AND idgrupo='$grupoSelec'";
+            }
+            if($discoSelec){
+                $sql .= "AND iddisco='$discoSelec'";
+            }
+            
+            $consulta = mysqli_query($conex, $sql);
+            $datos = mysqli_fetch_array($consulta);
+        
+    ?>
+
+
+</body>
+</html>
