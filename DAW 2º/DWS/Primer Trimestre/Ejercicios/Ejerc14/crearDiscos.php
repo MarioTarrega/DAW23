855615bfd635c6d5069de3fb60b287c91f13fade
MarioTarrega <145040216+MarioTarrega@users.noreply.github.com>
2024-01-15 18:26:24 +0000

    1

--- DAW 2º/DWS/Primer Trimestre/Ejercicios/Ejerc14/crearDiscos.php
+++ DAW 2º/DWS/Primer Trimestre/Ejercicios/Ejerc14/crearDiscos.php
@@ -4,12 +4,15 @@
 
     $conex = conectarBBDD();
 
-    if (isset($_POST['nombre'])) {
+    if (isset($_SESSION['nombre'])) {
         $usuario = $_SESSION['nombre'];
         echo "$usuario vamos a crear un disco";
 
         $sql = "SELECT * FROM grupos";
         $grupos = mysqli_query($conex,$sql);
+
+       $sqlS = "SELECT * FROM sellos";
+       $sellos = mysqli_query($conex, $sqlS);
     }
 ?>
 <!DOCTYPE html>
@@ -21,26 +24,37 @@
 </head>
 <body>
     
-    <form action="crearDiscos.php" method="post">
+    <form action="gestionDiscos.php" method="post">
         <label for="nombre">Nombre: </label>
         <input type="text" name="nombreDisco" id="nombreDisco">
         <label for="año">Año:</label>
         <input type="text" name="anyoSalida" id="anyoSalida">
         <label for="Sello">Sello:</label>
         <select name="Sello" id="sello">
-            <option value="Mute Record">Mute Record</option>
-            <option value="Warner Bros">Warner Bros</option>
-            <option value="Independiente">Independiente</option>
+        <option value="Grupo">Selecciona Sello</option>
+                <?php
+                    while($arr = mysqli_fetch_array($sellos)){
+                        echo '<option value='.$arr[0].'>'.$arr[1].'</option>';
+                    }
+                ?>
         </select>
         <br><br>
-        <select name="CrearG" id="crear-disco">
-            <option value="Grupo">Selecciona grupo</option>
+        <label for="Grupo">Grupo:</label>
+        <select name="grupoMost" id="grupo-mostrar">
+            <option value="Grupo">Selecciona Grupo</option>
                 <?php
                     while($arr = mysqli_fetch_array($grupos)){
                         echo '<option value='.$arr[0].'>'.$arr[1].'</option>';
                     }
                 ?>
         </select>
+        <label for="Cubierta">Cubierta:</label>
+        <input type="file" name="cubierta" id="cubierta-disco">
+        <label for="Cantidad_Canciones">Cantidad de Canciones</label>
+        <input type="number" min="1" name="canciones" id="numero">
+        <br>
+        <label for="Descripcion">Descripción:</label>
+        <textarea name="descripcion" id="descripcion-disco" cols="30" rows="10"></textarea>
     </form>
 </body>
 </html>
